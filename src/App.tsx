0818617d35b9b1437292293df7bdea1250248097
Jean Augustin <jre.augustin@gmail.com>
2026-09-26 00:54:30 -0400

    created initial sections: header, projects, github activity

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,24 @@ function App() {
 
   return (
     <>
+      <header id="portfolio-header">
+        <h1>Jean R. Augustin</h1>
+        <h5>Software Engineer · Musician · Founder</h5>
+        <p>Building software and products at the intersection of technology, music, and creativity.</p>
+      </header>
 
+
+      <div id="portfolio-projects">
+        <h4>Recent Projects</h4>
+
+
+      </div>
       <div id="github-activity">
         <h4>Follow @BeatsNCode on Github</h4>
-        <GitHubCalendar username="BeatsNCode" />
+        <GitHubCalendar 
+          username="BeatsNCode"
+          blockSize={11}
+        />
       </div>
     </>
   )
